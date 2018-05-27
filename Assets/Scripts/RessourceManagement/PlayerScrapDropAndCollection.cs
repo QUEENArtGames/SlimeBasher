08ae7b847c7dff6049ae4a5846df5c3373f0f63d
PinkPonyKnight <32565920+PinkPonyKnight@users.noreply.github.com>
2018-05-27 14:11:39 +0200

    SpielerskriptDummyAuslagerung

--- Assets/Scripts/RessourceManagement/PlayerScrapDropAndCollection.cs
+++ Assets/Scripts/RessourceManagement/PlayerScrapDropAndCollection.cs
@@ -6,81 +6,14 @@ public class PlayerScrapDropAndCollection : MonoBehaviour
 {
     private PlayerScrapInventory _scrapInventory;
     private RessourceManagement _ressourceManagement;
-    private TowerBuilding _towerBuilding;
-    private float _droprate;
 
     void Awake()
     {
         _scrapInventory = gameObject.GetComponent<PlayerScrapInventory>();
         _ressourceManagement = FindObjectOfType<RessourceManagement>();
-        _droprate = _ressourceManagement.PlayerScrapDropProbabilityInPercent;
-        _towerBuilding = FindObjectOfType<TowerBuilding>();
     }
 
-    //---------------------------- Testfunktionen Beginn
-    void Update()
-    {
-        if (Input.GetKeyDown("o"))
-            DropScraps();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        //TowerBuildingUI nur einmal global
-        //Spielerskript
-        if (other.transform.gameObject.CompareTag("Tower") /*&& DefaultMode*/)
-        {
-            FindObjectOfType<TowerBuildingUI>().ShowTowerKillNotification();
-            TowerRessourceManagement towermanagement = other.transform.gameObject.GetComponent<TowerRessourceManagement>();
-            if (_towerBuilding.CheckForRessources(_scrapInventory.ScrapInventory, towermanagement) && towermanagement.ScrapSlotsOnTowerAreFree())
-                FindObjectOfType<TowerBuildingUI>().ShowTowerUpgraeNotification();
-        }
-        ////
-            
-
-        if (other.transform.gameObject.CompareTag("Scrap"))
-            CollectScrap(other.transform.gameObject);
-    }
-
-    //TESTEREI
-    private void OnTriggerStay(Collider other)
-    {
-        //Spielerskript
-        if (other.transform.gameObject.CompareTag("Tower") && Input.GetKeyDown("u") /*&& DefaultMode*/)
-        {
-            FindObjectOfType<TowerBuildingUI>().OpenTowerBuildingMenu(other.gameObject);
-        }
-
-        if (other.transform.gameObject.CompareTag("Tower") && Input.GetKeyDown("k") /*&& DefaultMode*/)
-        {
-            FindObjectOfType<TowerBuildingUI>().CloseTowerUpgradeNotification();
-            FindObjectOfType<TowerBuildingUI>().CloseTowerBuildingMenu();
-            other.gameObject.GetComponent<Tower>().Kill();
-        }
-
-        if (other.transform.gameObject.CompareTag("Tower") && Input.GetKeyDown("q") /*&& DefaultMode*/)
-        {
-            FindObjectOfType<TowerBuildingUI>().CloseTowerUpgradeNotification();
-            FindObjectOfType<TowerBuildingUI>().CloseTowerBuildingMenu();
-            FindObjectOfType<TowerBuilding>().UpgradeWithAnyScrap(other.gameObject.GetComponent<TowerRessourceManagement>());
-        }
-    }
-
-    //TESTEREI
-    private void OnTriggerExit(Collider other)
-    {
-        //Spielerskript
-        if (other.transform.gameObject.CompareTag("Tower") /*&& DefaultMode*/)
-        {
-            FindObjectOfType<TowerBuildingUI>().CloseTowerKillNotification();
-            FindObjectOfType<TowerBuildingUI>().CloseTowerUpgradeNotification();
-            FindObjectOfType<TowerBuildingUI>().CloseTowerBuildingMenu();
-        }
-    }
-
-    //---------------------------- Testfunktionen Ende
-
-    private void CollectScrap(GameObject scrapObject)
+    public void CollectScrap(GameObject scrapObject)
     {
         Scrap scrap = scrapObject.GetComponent<Scrap>(); 
         if (!scrap.IsCollected)
@@ -97,7 +30,7 @@ public class PlayerScrapDropAndCollection : MonoBehaviour
         {
             for (int index = 0; index < _scrapInventory.ScrapInventory[scrapTypeIndex].Count; index++)
             {
-                if (Random.Range(0.0f, 100.0f) < _droprate)
+                if (Random.Range(0.0f, 100.0f) < _ressourceManagement.PlayerScrapDropProbabilityInPercent)
                 {
                     GameObject scrapObject = Instantiate(_ressourceManagement.GetScrapPrefabBySubTypeIndex(scrapTypeIndex, _scrapInventory.ScrapInventory[scrapTypeIndex][index]), transform.position, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f));
                     Scrap scrap = scrapObject.GetComponent<Scrap>();
---
+++ Assets/Scripts/RessourceManagement/TestScript/PlayerDummy.cs
@@ -0,0 +1,83 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDummy : MonoBehaviour {
+    PlayerScrapDropAndCollection _playerScrapDropAndCollection;
+    TowerBuilding _towerBuilding;
+    PlayerScrapInventory _scrapInventory;
+    TowerBuildingUI _towerBuildingUI;
+
+    void Awake()
+    {
+        _playerScrapDropAndCollection = GetComponent<PlayerScrapDropAndCollection>();
+        _towerBuilding = FindObjectOfType<TowerBuilding>();
+        _scrapInventory = GetComponent<PlayerScrapInventory>();
+        _towerBuildingUI = FindObjectOfType<TowerBuildingUI>();
+
+    }
+
+    //---------------------------- Testfunktionen Beginn
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+            _playerScrapDropAndCollection.DropScraps();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //TowerBuildingUI nur einmal global
+        //Spielerskript
+        if (other.transform.gameObject.CompareTag("Tower") /*&& DefaultMode*/)
+        {
+            _towerBuildingUI.ShowTowerKillNotification();
+            TowerRessourceManagement towermanagement = other.transform.gameObject.GetComponent<TowerRessourceManagement>();
+            if (_towerBuilding.CheckForRessources(_scrapInventory.ScrapInventory, towermanagement) && towermanagement.ScrapSlotsOnTowerAreFree())
+                _towerBuildingUI.ShowTowerUpgraeNotification();
+        }
+        ////
+
+
+        if (other.transform.gameObject.CompareTag("Scrap"))
+            _playerScrapDropAndCollection.CollectScrap(other.transform.gameObject);
+    }
+
+    //TESTEREI
+    private void OnTriggerStay(Collider other)
+    {
+        //Spielerskript
+        if (other.transform.gameObject.CompareTag("Tower") && Input.GetKeyDown("u") /*&& DefaultMode*/)
+        {
+            _towerBuildingUI.OpenTowerBuildingMenu(other.gameObject);
+        }
+
+        if (other.transform.gameObject.CompareTag("Tower") && Input.GetKeyDown("k") /*&& DefaultMode*/)
+        {
+            _towerBuildingUI.CloseTowerUpgradeNotification();
+            _towerBuildingUI.CloseTowerBuildingMenu();
+            other.gameObject.GetComponent<Tower>().Kill();
+        }
+
+        if (other.transform.gameObject.CompareTag("Tower") && Input.GetKeyDown("q") /*&& DefaultMode*/)
+        {
+            _towerBuildingUI.CloseTowerUpgradeNotification();
+            _towerBuildingUI.CloseTowerBuildingMenu();
+            _towerBuilding.UpgradeWithAnyScrap(other.gameObject.GetComponent<TowerRessourceManagement>());
+        }
+    }
+
+    //TESTEREI
+    private void OnTriggerExit(Collider other)
+    {
+        //Spielerskript
+        if (other.transform.gameObject.CompareTag("Tower") /*&& DefaultMode*/)
+        {
+            _towerBuildingUI.CloseTowerKillNotification();
+            _towerBuildingUI.CloseTowerUpgradeNotification();
+            _towerBuildingUI.CloseTowerBuildingMenu();
+        }
+    }
+
+    //---------------------------- Testfunktionen Ende
+}
