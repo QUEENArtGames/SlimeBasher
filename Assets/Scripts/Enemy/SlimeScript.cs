5b560c5549d38cad3fed7ae3af571f13c1322db9
Tim | Axsem <timg040497@gmail.com>
2018-06-12 23:50:47 +0200

    Slime attack Tower

--- Assets/Scripts/Enemy/SlimeScript.cs
+++ Assets/Scripts/Enemy/SlimeScript.cs
@@ -8,20 +8,68 @@ public class SlimeScript : MonoBehaviour {
 
 	public float _hp;
 	public float _damage;
+	public float _attackSpeed;
 	public Transform _targetLocation;
+	public float _aggroRange;
 
-	private NavMeshAgent _navMeshAgent; 
+	public TowerManagerTestScript towerManagerTestScript;
 
+	private NavMeshAgent _navMeshAgent;
+	private GameObject _tmpTarget;
+
+	private float _nextAttack = 0;
 	// Use this for initialization
 	void Start () {
 
+
 		_navMeshAgent = GetComponent<NavMeshAgent> ();
+		_navMeshAgent.stoppingDistance = 2;
 		setTargetLocation ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		checkAggro ();
+
+		if (_tmpTarget != null) {
+			attackTower ();
+		}
+	}
 		
+
+	public void checkAggro(){
+
+		float shortestDistance = _aggroRange+1;
+		bool newTarget = false;
+		foreach (GameObject tower in towerManagerTestScript.towers) {
+			float distance = Vector3.Distance (transform.position, tower.transform.position);
+			if (distance < _aggroRange) {
+
+				if(newTarget == false || distance < shortestDistance){
+					newTarget = true;
+					shortestDistance = distance;
+					_tmpTarget = tower;
+				}
+			}
+		}
+
+		if (newTarget) {
+			_navMeshAgent.SetDestination (_tmpTarget.transform.position);
+
+		} else if(_tmpTarget != null){
+			_tmpTarget = null;
+			setTargetLocation ();
+		}
+
+	}
+
+	public void attackTower(){
+		if (Time.time > _nextAttack) {
+			Tower tower = _tmpTarget.transform.GetComponent<Tower>();
+			tower.attacked(_damage);
+
+			_nextAttack = Time.time + _attackSpeed;
+		}
 	}
 
 	void OnCollisionEnter (Collision col)
--- Assets/Scripts/TowerPlacement/Tower.cs
+++ Assets/Scripts/TowerPlacement/Tower.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Tower : MonoBehaviour
 {
+
+	public float _hp;
+
     private List<Collider> _colliders = new List<Collider>();
     private List<Renderer> _renderers = new List<Renderer>();
     private Color _colorPlaceable = new Color(0, 1, 0, 0.5f);
@@ -60,6 +63,13 @@ public class Tower : MonoBehaviour
             }
         }
     }
+
+	public void attacked(float damage){
+		_hp -= damage;
+		if (_hp <= 0) {
+			//TODO: destory + remove from List
+		}
+	}
 
     internal void Kill()
     {
