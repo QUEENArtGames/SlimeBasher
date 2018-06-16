35bfd7ce5a7016eee19f1c0cd1473551ce44b55d
Zitrone <mueller.sven-@outlook.de>
2018-06-16 14:05:14 +0200

    bubble tower prototype finished

---
+++ Assets/Scripts/Tower/BubbleTower/BubbleProjectileScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleProjectileScript : MonoBehaviour {
+
+
+	public bool colliderIsEnabled = true;
+	public float angle;
+    public Transform spawnpoint;
+    public GameObject splashEffect;
+
+    public float maxScale = 2;
+    private float growSpeed;
+    private float tremble = 0;
+
+    public float speed = 1f;
+	public float range = 5;
+
+	private Vector3 startPosition;
+
+	void Start () {
+		transform.eulerAngles = new Vector3(
+			transform.eulerAngles.x,
+			angle + 90,
+			transform.eulerAngles.z
+		);
+		startPosition = transform.position;
+        growSpeed = maxScale / 80;
+	}
+	
+	void Update () {  
+
+		float dist = Vector3.Distance(startPosition, transform.position);
+		//print("Distance to other: " + dist);
+		if(dist >range){
+			Debug.Log(dist);
+            var splash = Instantiate(splashEffect, transform.position, transform.rotation);
+            Destroy(splash.gameObject, 1);
+            Destroy(gameObject);
+        }
+        //Debug.Log(startPosition.x-transform);
+
+        GrowAndMove();  
+    }
+
+    public void GrowAndMove()
+    {
+        if (transform.localScale.x <= maxScale)
+        {
+            //tremble = (Random.value - 0.5f) * 0.01f;
+
+            transform.localPosition = new Vector3(spawnpoint.position.x + tremble, spawnpoint.position.y + tremble, spawnpoint.position.z + tremble) + 
+                                                 (spawnpoint.forward * transform.localScale.x / 2);
+            transform.localScale += new Vector3(growSpeed, growSpeed, growSpeed);   
+        }
+        else
+        {
+            transform.localPosition += transform.forward * speed;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Tower/BubbleTower/BubbleTowerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleTowerScript : MonoBehaviour {
+
+	//public Transform Target;
+
+	public GameObject projectile;
+
+    public float spawnDelay = 0f;
+
+	public float angle;
+
+	void Start () {
+        //StartCoroutine(BubbleProjectile());
+	}
+	
+	void Update () {
+		
+	}
+
+	IEnumerator BubbleProjectile(){
+		
+		yield return new WaitForSeconds(spawnDelay);
+
+		//StartCoroutine(BubbleProjectile());
+		GameObject bubbleProjectile = Instantiate(projectile) as GameObject;
+        //print(transform.eulerAngles.y);
+
+        bubbleProjectile.GetComponent<BubbleProjectileScript>().angle = transform.eulerAngles.y;
+		Physics.IgnoreCollision(bubbleProjectile.GetComponent<Collider>(), GetComponent<Collider>());
+
+        //bubbleProjectile.transform.position = transform.position + new Vector3(0.1f, 0.5f, 0f);
+        bubbleProjectile.GetComponent<BubbleProjectileScript>().spawnpoint = transform.GetChild(1).transform;
+
+    }
+
+}
