fc17c99c5211227d54242f5c87479ee732e85cde
Lennart-PC\Lennart <lennartkaufmann0@gmail.com>
2018-07-05 22:49:31 +0200

    new particle effect plays when a tidepod collides with the ground, balanced shoot angle of the bass-tower

--- Assets/Scripts/Tower/throwtower/PuddleScript.cs
+++ Assets/Scripts/Tower/throwtower/PuddleScript.cs
@@ -1,17 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 namespace Assets.Scripts
 {
     public class PuddleScript : MonoBehaviour
     {
 
         public float _damage = 10;
-        public float _livetime = 0.5f;
+        public float _livetime = 7f;
+        public LayerMask IgnoreMe;
+        public GameObject toxicGas;
 
         private float currundtime = 0;
         // Use this for initialization
         void Start()
         {
-
+            StartCoroutine(SpawnParticleEffect());
         }
 
         // Update is called once per frame
@@ -32,6 +35,26 @@ namespace Assets.Scripts
             {
 
                 other.gameObject.GetComponent<SlimeScript>().TakeDamage(_damage);
+            }
+        }
+
+        IEnumerator SpawnParticleEffect()
+        {
+            Vector3 transformOffsetSpawn = transform.position;
+            //transformOffsetSpawn.y += 10f;
+
+            RaycastHit hit;
+            if (Physics.Raycast(transformOffsetSpawn, Vector3.down, out hit, 1000f, ~IgnoreMe))
+            {
+                GameObject particle = Instantiate(toxicGas, hit.point, toxicGas.transform.rotation) as GameObject;
+                ParticleSystem parSystem = particle.GetComponent<ParticleSystem>();
+                parSystem.Play();
+
+                yield return new WaitForSeconds(3);
+                Debug.Log("DURATION: " + parSystem.main.duration);
+                parSystem.Stop();
+                Destroy(particle, parSystem.main.duration + 3.0f);
+
             }
         }
 
