d2308cdad78af4667c0f592e68750f5579d8b9a1
Lennart-PC\Lennart <lennartkaufmann0@gmail.com>
2018-07-06 01:58:41 +0200

    added meele-tower damage, fixed no reference problem in bass-tower script, bass-tower damage nerf

---
+++ Assets/Scripts/Tower/MeleeTower/Attack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Attack : MonoBehaviour
+    {
+        private float damage = 17.0f;
+        public bool dealDamage = false;
+
+        private void Update()
+        {
+            if (transform.parent != null && transform.parent.parent != null)
+            {
+                if (transform.parent.parent.name == "Holder3")
+                {
+                    dealDamage = true;
+                    GetComponent<Collider>().isTrigger = true;
+                }
+            }
+            else
+            {
+                dealDamage = false;
+                GetComponent<Collider>().isTrigger = false;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            //Debug.Log("dealDamage: " + dealDamage + " is enemey: " + other.gameObject.tag == "Enemy");
+            if (other.gameObject.tag == "Enemy" && dealDamage)
+            {
+                other.gameObject.GetComponent<SlimeScript>().TakeDamage(damage);
+            }
+        }
+    }
+}
